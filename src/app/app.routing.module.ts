bfde6de309ff771765dcd7ce7224f68d6ceaa723
andras <fejes.andras@dev-labs.com>
2020-10-22 23:13:52 +0200

    :lipstick: Add ECG image

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {BpmDashboardComponent} from './bpm-dashboard/containers/bpm-dashboard/bpm-dashboard.component';
 import {BpmEcgComponent} from './bpm-dashboard/components/bpm-ecg/bpm-ecg.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: BpmDashboardComponent },
-  { path: 'ecg/:id/:time', component: BpmEcgComponent },
-  { path: '**',   redirectTo: '/dashboard', pathMatch: 'full' }
+  {path: 'dashboard', component: BpmDashboardComponent},
+  {path: 'ecg/:id/:time', component: BpmEcgComponent},
+  {path: '**', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
